ad6edf48415e5d3f72fb33a8d977c8a68e54df8c
Fandr1d1s <118404173+Fandr1d1s@users.noreply.github.com>
2024-12-05 04:55:46 +0500

    Add files via upload

---
+++ Converter.scala
@@ -0,0 +1,29 @@
+package SomeTasks
+
+trait DollarConversion {
+  def get: BigDecimal
+  def toRub(exchangeRate: BigDecimal) = new Ruble(this.get * exchangeRate)
+  def toDin(exchangeRate: BigDecimal) = new Dinar(this.get * exchangeRate)
+}
+trait DinarConversion {
+  def get: BigDecimal
+  def toRub(exchangeRate: BigDecimal) = new Ruble(this.get * exchangeRate)
+  def toDol(exchangeRate: BigDecimal) = new Dollar(this.get * exchangeRate)
+}
+class Ruble(private val value: BigDecimal) {
+  def get: BigDecimal = value
+  override def toString: String = s"Ruble($value)"
+}
+class Dollar(private val value: BigDecimal) extends DollarConversion {
+  def get: BigDecimal = value
+  override def toString: String = s"Dollar($value)"
+}
+class Dinar(private val value: BigDecimal) extends DinarConversion {
+  def get: BigDecimal = value
+  override def toString: String = s"Dinar($value)"
+}
+
+object TestConversions extends App {
+  println(new Dollar(10).toRub(104.47))
+  println(new Dollar(10).toDin(0.38).toRub(277.6))
+}
